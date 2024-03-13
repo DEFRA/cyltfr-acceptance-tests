4c49f5d7ea13f00897ec133376d5aa22e90f2430
Harish Kancharla <harish.kancharla@gmail.com>
2024-03-13 11:38:49 +0000

    LTFRI-727: acceptances tests added

--- test/specs/page_objects/postcode_page.js
+++ test/specs/page_objects/postcode_page.js
@@ -1,37 +1,80 @@
-'use strict'
+"use strict";
 class PostcodePage {
   // LOCATORS
-  get title () { return $('h1') }
-  get subHeading () { return $("//p[contains(text(),'This service will tell you the flood risk of an ar')]") }
-  get postcodeHeading () { return $("//label[normalize-space()='Enter a postcode']") }
-  get postcodeHint () { return $('#postcode-hint') }
-  get postcodeTextbox () { return $('#postcode') }
-  get postcodeError () { return $('#postcode-error') }
-  get contBtn () { return $('#post-code-button') }
-  get viewMapAreasFloodingLink () { return $("//a[@href='/map']") }
-  get royalMailPostcodeFinderLink () { return $("//a[@href='http://www.royalmail.com/find-a-postcode']") }
-  get bannerError () { return $("ul[class='govuk-list govuk-error-summary__list']") }
+  get title() {
+    return $("h1");
+  }
+  get subHeading() {
+    return $(
+      "//p[contains(text(),'This service will tell you the flood risk of an ar')]"
+    );
+  }
+  get postcodeHeading() {
+    return $("//label[normalize-space()='Enter a postcode']");
+  }
+  get postcodeHint() {
+    return $("#postcode-hint");
+  }
+  get postcodeTextbox() {
+    return $("#postcode");
+  }
+  get postcodeError() {
+    return $("#postcode-error");
+  }
+  get contBtn() {
+    return $("#post-code-button");
+  }
+  get viewMapAreasFloodingLink() {
+    return $("//a[@href='/map']");
+  }
+  get royalMailPostcodeFinderLink() {
+    return $("//a[@href='http://www.royalmail.com/find-a-postcode']");
+  }
+  get bannerError() {
+    return $("[class='govuk-list govuk-error-summary__list']");
+  }
+  get acceptCookieBtn() {
+    return $("[class='govuk-button js-cookies-button-accept']");
+  }
+  get hideCookieBtn() {
+    return $(
+      "body > div.js-cookies-container > form > div > div.govuk-cookie-banner__message.js-cookies-accepted.govuk-width-container > div.govuk-button-group > a"
+    );
+  }
 
   // METHODS FUNCTIONS ACTIONS
 
-  async enterPostcode (postcode) {
-    await (await this.postcodeTextbox).waitForDisplayed({})
-    return (await this.postcodeTextbox).setValue(postcode)
+  async enterPostcode(postcode) {
+    await (await this.postcodeTextbox).waitForDisplayed({});
+    return (await this.postcodeTextbox).setValue(postcode);
   }
 
-  async clickContinue () {
-    await (await this.contBtn).waitForDisplayed({})
-    return (await this.contBtn).click()
+  async clickContinue() {
+    await (await this.contBtn).waitForDisplayed({});
+    return (await this.contBtn).click();
   }
 
-  async getPostcodeErrorMessage () {
-    await (await this.postcodeError).waitForDisplayed({})
-    return (await this.postcodeError).getText()
+  async getPostcodeErrorMessage() {
+    await (await this.postcodeError).waitForDisplayed({});
+    return (await this.postcodeError).getText();
   }
 
-  async getPostcodeBannerMessage () {
-    await (await this.bannerError).waitForDisplayed({})
-    return (await this.bannerError).getText()
+  async getPostcodeBannerMessage() {
+    await (await this.bannerError).waitForDisplayed({});
+    return (await this.bannerError).getText();
+  }
+
+  async acceptCookies() {
+    try {
+      await (await this.acceptCookieBtn).click();
+      await (await this.hideCookieBtn).click();
+    } catch (err) {
+      console.log("Element not present");
+    }
+    // if (await this.acceptCookieBtn.isExisting()) {
+    //   await (await this.acceptCookieBtn).click();
+    //   await (await this.hideCookieBtn).click();
+    // }
   }
 }
-module.exports = new PostcodePage()
+module.exports = new PostcodePage();
---
+++ test/specs/test_data/reservoir_map_data.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = {
+  reservoirData: [
+    {
+      testCase: 0,
+      houseNumber: 5,
+      postcode: "SY1 2BL",
+      dropDownValue: "2",
+      riverAndSeaRisk: "Very low risk",
+      surfaceWaterRisk: "Very low risk",
+      reservoirRisk: false,
+      groundwaterRisk: false,
+    },
+    {
+      testCase: 1,
+      houseNumber: 5,
+      postcode: "SY10 8PB",
+      dropDownValue: "2",
+      riverAndSeaRisk: "Medium risk",
+      surfaceWaterRisk: "Very low risk",
+      reservoirRisk: false,
+      groundwaterRisk: false,
+    },
+    {
+      testCase: 2,
+      houseNumber: 5,
+      postcode: "LN8 2DX",
+      dropDownValue: "1",
+      riverAndSeaRisk: "Very low risk",
+      surfaceWaterRisk: "Very low risk",
+      reservoirRisk: false,
+      groundwaterRisk: false,
+    },
+  ],
+};
