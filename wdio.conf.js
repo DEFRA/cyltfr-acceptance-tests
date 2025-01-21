ca575c61a578a1a7c8565e6bd5e99b0d217f9613
Matthew Collins <matthew.collins@defra.gov.uk>
2025-01-21 09:20:14 +0000

    Try changing a timeout, see if it helps Jenkins

--- wdio.conf.js
+++ wdio.conf.js
@@ -115,7 +115,7 @@ exports.config = {
   baseUrl: global.baseUrl,
 
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 10000,
+  waitforTimeout: 40000,
   //
   // Default timeout in milliseconds for request
   // if browser driver or grid doesn't send response
